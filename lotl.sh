227ce33d08db179bafcc63b11e4d3129a49f3ec5
Kenan Rhoton <kenan.rhoton@gmail.com>
2015-11-03 16:20:00 +0100

    Grow!

--- lotl.sh
+++ lotl.sh
@@ -8,10 +8,15 @@ FACTION=0
 
 declare -a FAMILY
 FACTION_OPTIONS="\"Nomads\""
-BASIC_OPTIONS="Grow"
+BASIC_OPTIONS="\"Grow\""
 GROW_OPTIONS=""
 
 MY_CIV=""
+
+declare -a OPTION_REQS
+OPTION_REQS+=("Hunters Grow none Nomads")
+OPTION_REQS+=("Gatherers Grow none Nomads")
+OPTION_REQS+=("Warriors Grow none Hunters")
 
 lotlChoiceAction(){
     _RET=""
@@ -40,14 +45,31 @@ lotlChoiceAction(){
     done
 }
 
+lotlCheckArray(){
+    _RET=0
+    echo $@
+    echo ${*[-1]}
+    echo ${@[1]}
+    for value in ${@%${@[-1]}}
+    do
+        echo $value == $2
+        if [[ "$value" == "$2" ]]
+        then
+            _RET=1
+        fi
+    done
+    exit
+}
+
 lotlFactionSelect(){
     lotlChoiceAction "Choose your faction:" $FACTION_OPTIONS
-    FACTION=$_RET
+    #FACTION=$_RET
+    MY_CIV+=$_RET
 }
 
 lotlStartingFamilies(){
     case "$1" in
-        "Nomads") _RET="\"Hunters\" \"Gatherers\""
+        "\"Nomads\"") _RET="\"Hunters\" \"Gatherers\""
             ;;
         "Settled") _RET="\"Builders\" \"Farmers\" \"Villagers\""
             ;;
@@ -55,10 +77,63 @@ lotlStartingFamilies(){
     esac
 }
 
+lotlRemoveGrowOption(){
+    lotlCheckArray $GROW_OPTIONS $1
+    if [[ $_RET -eq 1 ]]
+    then
+        GROW_OPTIONS=${GROW_OPTIONS#$1}
+    fi
+}
+
+lotlAddCiv(){
+    MY_CIV+=" $1"
+}
+
+lotlEvaluateGrow(){
+    #if $1 is in $MY_CIV do nothing
+    if ! [[ $MY_CIV =~ "$1" ]]
+    then
+        grow=$1
+        shift
+        shift
+        shift
+        put="true"
+        for i in $@
+        do
+            if ! [[ $MY_CIV =~ "$i" ]]
+            then
+                put="false"
+            fi
+        done
+        if [[ $put == "true" ]]
+        then
+            GROW_OPTIONS+=" $grow"
+        fi
+    fi
+    #else if $3 is not none store the action
+    #and if all REQS $4,$5,$6,$7... are in $MY_CIV then add to GROW_OPTIONS
+}
+
+lotlEvaluateReq(){
+    case "$2" in 
+        "Grow") lotlEvaluateGrow $@
+            ;;
+    esac
+}
+
+lotlEvaluateCiv(){
+    GROW_OPTIONS=""
+    for req in `seq 0 ${#OPTION_REQS[*]}`
+    do
+       lotlEvaluateReq ${OPTION_REQS[req]}
+      
+    done
+}
+
 
 
 lotlGrowAction(){
-    lotlRemoveGrowOption "$1"
+    #lotlRemoveGrowOption "$1"
     lotlAddCiv "$1"
 }
 
@@ -66,25 +141,25 @@ lotlFamilySelect(){
     lotlStartingFamilies $FACTION
     GROW_OPTIONS=$_RET
     lotlChoiceAction "You chose the $FACTION!\nChoose your starting family:" $GROW_OPTIONS
-    case "$_RET" in
-        1) 
-    esac
+    lotlGrowAction $_RET
 }
 
 
 lotlExecuteAction(){
     case "$1" in
-        "Grow") lotlGrowAction
+        "\"Grow\"") lotlChoiceAction "Choose how to grow:" $GROW_OPTIONS
+            lotlGrowAction $_RET
     esac
 }
 
 lotlNextTurn(){
+    lotlEvaluateCiv
     lotlChoiceAction "Choose an Action:" $BASIC_OPTIONS
     lotlExecuteAction $_RET
 }
 
 lotlStartGame(){
-    while [[ -n $ENDGAME ]]
+    while [[ -z $ENDGAME ]]
     do
         lotlNextTurn
     done
@@ -93,7 +168,7 @@ lotlStartGame(){
 
 lotlNewGame(){
     lotlFactionSelect
-    lotlFamilySelect
+    #lotlFamilySelect
     lotlStartGame
 }
 
